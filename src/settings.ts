bbb8c1bf624976ac9a334743654959a949ddfe9c
Adetoun Favour <favouradetoun@gmail.com>
2026-01-01 17:33:20 +0100

    remove unused setting

--- src/settings.ts
+++ src/settings.ts
@@ -2,4 +2,3 @@ export const MARKET_FLIP_COUNT  = 2;
 export const PILLAR_FACEUP_OFFSET = "20px";
 export const PILLAR_FACEDOWN_OFFSET = "10px";
 export const MARKET_OFFSET = "20px";
-export const PILLAR_OFFSET = "10px";
