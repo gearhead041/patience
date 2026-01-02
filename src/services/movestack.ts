93bae99d554bb3bb12b9a1735c75b658035d0ace
Adetoun Favour <favouradetoun@gmail.com>
2026-01-02 14:55:45 +0100

    fix movestack

--- src/services/movestack.ts
+++ src/services/movestack.ts
@@ -1,17 +1,19 @@
 export class MoveStack<T> {
-	private readonly moveStack: T[];
+	private readonly moveStack: T[] = [];
 	private readonly capacity: number;
 	private readonly undoneStack: T[] = [];
 
 	constructor(capacity: number) {
-		this.capacity = capacity +1; //because one slot is unused to differentiate full vs empty
-		this.moveStack = new Array(this.capacity);
+		this.capacity = capacity;
 	}
 
 	push(state: T): void {
 		this.moveStack.push(state);
 		if (this.moveStack.length > this.capacity) {
 			this.moveStack.shift();
+		}
+		if( this.undoneStack.length > 0) {
+			this.undoneStack.length = 0; //clear redo stack on new move
 		}
 	}
 
